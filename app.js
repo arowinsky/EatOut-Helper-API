dac82383b6d1e987213a559f0957c858cd92e809
Artur Rowinski <arowinski96@gmail.com>
2019-10-15 13:07:41 +0200

    Add error handling when email unverified

--- app.js
+++ app.js
@@ -31,59 +31,69 @@ app.post("/loginEmail", (req, res) => {
       authData
     )
     .then(user => {
-      console.log("get user");
-      req.session.Token = user.data.idToken;
-      req.session.email = user.data.email;
-      req.session.localId = user.data.localId;
-      req.session.expiresIn = user.data.expiresIn;
-    })
-    .then(() => {
-      db.collection("users")
-        .doc(req.session.localId)
-        .get()
-        .then(doc => {
-          let userData = doc.data().userData;
-          req.session.userData = userData;
-          console.log(req.sessionID);
-
-          res.status(201).json({
-            status: true,
-            name: userData,
-            idSession: req.sessionID
+      axios({
+        method: "POST",
+        url:
+          "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyAaJRfgtMU3LqvV07NyiaGfqUj_XGpkoNo",
+        data: {
+          idToken: user.data.idToken
+        }
+      }).then(users => {
+        console.log(users.data.users[0].emailVerified);
+        if (users.data.users[0].emailVerified === false) {
+          res.json({
+            emailUnverified: "unverified"
           });
-        });
+        } else {
+          req.session.Token = user.data.idToken;
+          req.session.email = user.data.email;
+          req.session.localId = user.data.localId;
+          req.session.expiresIn = user.data.expiresIn;
+          db.collection("users")
+            .doc(req.session.localId)
+            .get()
+            .then(doc => {
+              let userData = doc.data().userData;
+              req.session.userData = userData;
+              console.log(req.sessionID);
+              res.json({
+                status: true,
+                name: userData,
+                idSession: req.sessionID
+              });
+            });
+        }
+      });
     })
     .catch(error => {
       res.status(201);
       res.json({ error: true });
     });
 }),
-  (module.exports = app);
-
-app.post("/autoLogin", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  const id = req.body.sid;
-  const reSend = req.body.pleaseReSend;
-  if (id != "null") {
-    store.get(id, (err, session) => {
-      if (id != "undefined" && reSend) {
-        console.log("Resending" + session.userData);
-        res.json({
-          userInfo: session.userData
-        });
-      }
-      if (err) {
-        console.log("error");
-        res.json({
-          session: "TimeOut"
-        });
-      }
-    });
-  } else {
-    console.log("lack sid");
-    res.json({
-      session: "TimeOut"
-    });
-  }
-});
+  app.post("/autoLogin", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    const id = req.body.sid;
+    const reSend = req.body.pleaseReSend;
+    if (id != "null") {
+      store.get(id, (err, session) => {
+        if (id != "undefined" && reSend) {
+          console.log("Resending" + session.userData);
+          res.json({
+            userInfo: session.userData
+          });
+        }
+        if (err) {
+          console.log("error");
+          res.json({
+            session: "TimeOut"
+          });
+        }
+      });
+    } else {
+      console.log("lack sid");
+      res.json({
+        session: "TimeOut"
+      });
+    }
+  });
 (module.exports = app), session;
