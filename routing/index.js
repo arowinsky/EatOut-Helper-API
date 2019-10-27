93c06de1f8fd05b2200b513d1cbe35f7e3623ce5
Artur Rowinski <arowinski96@gmail.com>
2019-10-27 15:09:11 +0100

    Reafactor in routing/index.js

--- routing/index.js
+++ routing/index.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
-const bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 const GoogleloginController = require("../controlers/GoogleLoginControles");
 const FbloginController = require("../controlers/FbLoginControles");
 const SessionLogin = require("../controlers/firebaseControles/sessionLogin");
 
-router.get("/", main.mailina);
-
 router.post("/sessionLogin", SessionLogin.sessionLogin);
-
 router.post("/loginGoogle", GoogleloginController.LoginGoogle);
 router.post("/loginFB", FbloginController.LoginFB);
 
