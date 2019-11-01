bba633fa6adf42d410db3d3ee2cc227648445c58
Artur Rowinski <arowinski96@gmail.com>
2019-11-01 14:32:42 +0100

    Fix error with receipt of data

--- controllers/addNewLocalController.js
+++ controllers/addNewLocalController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 router.post("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  const test = JSON.stringify(req.body.values);
+  const test = JSON.parse(req.body.values);
   console.log(test);
 });
 module.exports = router;
