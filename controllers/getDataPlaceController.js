11e744945f2f59a6abd4613fa26372468c9a4f42
Artur Rowinski <arowinski96@gmail.com>
2019-11-19 23:17:08 +0100

    Extended response in getDataPlaceController

--- controllers/getDataPlaceController.js
+++ controllers/getDataPlaceController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const redis = require("redis");
 const redisClient = redis.createClient();
-const {db, admin} = require("../config/firebaseConfig");
+const { db, admin } = require("../config/firebaseConfig");
 
 router.post("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,7 +15,7 @@ router.post("/", (req, res) => {
       const dates = JSON.parse(red);
       console.log(dates);
       console.log("local ", dates.localId);
-      db.collection("restaurants")
+      db.collection("eatingPlace")
         .where("info.owner", "==", dates.localId)
         .get()
         .then(snapshot => {
@@ -24,15 +24,19 @@ router.post("/", (req, res) => {
             return;
           }
           snapshot.forEach(doc => {
-            console.log(doc.id, "=>", doc.data().info);
+            const dishs = doc.data().dishs;
+            const info = doc.data().info;
+            const kitchen = doc.data().kitchen;
+            const opportunity = doc.data().opportunity;
+            const facilities = doc.data().facilities;
             const id = doc.id;
-            const eatingPlace = {
-              ...doc.data().data,
-              ...doc.data().info,
-              id
-            };
             res.json({
-              eatingPlace: eatingPlace
+              dishs: dishs,
+              info: info,
+              kitchen: kitchen,
+              opportunity: opportunity,
+              facilities: facilities,
+              id: id
             });
           });
         });
