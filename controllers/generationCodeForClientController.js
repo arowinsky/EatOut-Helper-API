86f5c0acedc28e605736135db37fb8f15529d08c
Artur Rowinski <arowinski96@gmail.com>
2019-11-26 20:47:02 +0100

    Add generation code for client

---
+++ controllers/generationCodeForClientController.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const { db, admin, storage } = require("../config/firebaseConfig");
+const randomstring = require("randomstring");
+
+router.post("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const { eatingPlaceId } = req.body;
+  console.log(eatingPlaceId);
+
+  const newCode = () => {
+    const codei = randomstring.generate({
+      length: 7,
+      capitalization: "lowercase"
+    });
+
+    db.collection("codes")
+      .where("code", "==", codei)
+      .get()
+      .then(docs => {
+        if (docs.size >= 1) {
+          newCode();
+        } else {
+          db.collection("codes")
+            .doc()
+            .set({
+              code: codei,
+              eatingPlaceId: eatingPlaceId
+            })
+            .then(() => {
+              res.json({
+                code: codei
+              });
+            });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  newCode();
+});
+module.exports = router;
