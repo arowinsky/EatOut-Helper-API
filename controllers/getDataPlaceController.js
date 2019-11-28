232071125e8c71cccf6b25388b9eb494bc936e69
MarekLandzwojczak <mareklandzwojczak1995@gmail.com>
2019-11-28 16:49:08 +0100

    add opinions client

--- controllers/getDataPlaceController.js
+++ controllers/getDataPlaceController.js
@@ -9,80 +9,74 @@ router.post("/", (req, res) => {
 
   console.log(req.body);
   const z = req.body.z;
-  if (z != "null") {
-    const key = "sess:" + z;
-    console.log(key);
-    redisClient.get(key, (error, red) => {
-      const dates = JSON.parse(red);
-      
-      
-      db.collection("eatingPlaces")
-        .where("info.owner", "==", dates.localId)
-        .get()
-        .then(snapshot => {
-          let Opinions;
-          if (snapshot.empty) {
-            console.log("No matching documents.");
-            return;
-          }else{
-            
-       const places = snapshot.docs.map(doc => {
-            const dishes = doc.data().dishes;
-            const info = doc.data().info;
-            const kitchen = doc.data().kitchen;
-            const opportunity = doc.data().opportunity;
-            const facilities = doc.data().facilities;
-            const id = doc.id;
-            const avatar = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/avatar.jpg`
-            const header = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/header.jpg`
-            const menu = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/menu.jpg`
 
-          db.collection("eatingPlaces").doc(id).collection('clientOpinions').orderBy('data').get().then((opinions)=>{
-            if(opinions.empty){
-              console.log('empty')
-            }
-            else{
-              Opinions = opinions.docs.map(opinion=>{
-                const author = opinion.data().author;
-                const clientOpinion = opinion.data().clientOpinion;
-                const date = opinion.data().data;
+  const key = "sess:" + z;
+  console.log(key);
+  redisClient.get(key, (error, red) => {
+    const dates = JSON.parse(red);
 
-                const SendOpinions = {
-                  author:author,
-                  clientOpinion: clientOpinion,
+    db.collection('eatingPlaces').where("info.owner", "==", dates.localId).get()
+      .then((places) => {
+
+        places.docs.map(doc => {
+
+
+          const dishes = doc.data().dishes;
+          const info = doc.data().info;
+          const kitchen = doc.data().kitchen;
+          const opportunity = doc.data().opportunity;
+          const facilities = doc.data().facilities;
+          const id = doc.id;
+          const avatar = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/avatar.jpg`
+          const header = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/header.jpg`
+          const menu = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/menu.jpg`
+
+
+
+          const opinione = db.collection('eatingPlaces').doc(id).collection('clientOpinions').get()
+            .then(Opinion => {
+
+              const opinion = Opinion.docs.map(docs => {
+
+                const author = docs.data().author;
+                const clientOpinion = docs.data().clientOpinion;
+                const date = docs.data().data;
+
+                const sendOpinin = {
+                  author: author,
+                  opinion: clientOpinion,
                   date: date
                 }
-                //console.log('opinie ', SendOpinions)
-                return(SendOpinions)
+                return (sendOpinin)
               })
-              console.log(Opinions)
-            }
 
-          })
-          
-            const array = {id:id,
-            dishes: dishes,
-            info: info,
-            kitchen: kitchen,
-            opportunity: opportunity,
-            facilities: facilities,
-            avatar: avatar,
-            header: header,
-            menu: menu,
-            }
-            
-            return(array)
-          
-          });
-          
-          console.log(places);
-          res.json({
-            places: places,
-            opinion: Opinions,
-          });
-        }
+              const array = {
+                id: id,
+                dishes: dishes,
+                info: info,
+                kitchen: kitchen,
+                opportunity: opportunity,
+                facilities: facilities,
+                avatar: avatar,
+                header: header,
+                menu: menu,
+                clientOpinions: opinion,
+              }
+
+              res.json({
+                places: array,
+              })
+
+            })
+
+
+
         });
-    });
-  }
+
+      })
+  });
 });
+
+
+
 module.exports = router;
