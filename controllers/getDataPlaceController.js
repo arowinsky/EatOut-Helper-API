11c8770c926002f13915296017a917d92700c770
Artur Rowinski <arowinski96@gmail.com>
2019-11-29 20:37:36 +0100

    Fix bug with sending data eatingPlaces

--- controllers/getDataPlaceController.js
+++ controllers/getDataPlaceController.js
@@ -15,57 +15,49 @@ router.post("/", (req, res) => {
   redisClient.get(key, (error, red) => {
     const dates = JSON.parse(red);
 
-    db.collection('eatingPlaces').where("info.owner", "==", dates.localId).get()
-      .then((places) => {
+    db.collection("eatingPlaces")
+      .where("info.owner", "==", dates.localId)
+      .get()
+      .then(places => {
+        if (places.empty) {
+          res.json({
+            places: false
+          });
+        } else {
+          const Places = places.docs.map(doc => {
+            const {
+              dishes,
+              info,
+              kitchen,
+              opportunity,
+              facilities
+            } = doc.data();
 
-        places.docs.map(doc => {
-          const dishes = doc.data().dishes;
-          const info = doc.data().info;
-          const kitchen = doc.data().kitchen;
-          const opportunity = doc.data().opportunity;
-          const facilities = doc.data().facilities;
-          const id = doc.id;
-          const avatar = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/avatar.jpg`
-          const header = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/header.jpg`
-          const menu = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/menu.jpg`
+            const id = doc.id;
+            const avatar = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/avatar.jpg`;
+            const header = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/header.jpg`;
+            const menu = `https://storage.cloud.google.com/eatout/${dates.localId}/${id}/menu.jpg`;
 
-          const opinione = db.collection('eatingPlaces').doc(id).collection('clientOpinions').get()
-            .then(Opinion => {
+            const array = {
+              id: id,
+              dishes: dishes,
+              info: info,
+              kitchen: kitchen,
+              opportunity: opportunity,
+              facilities: facilities,
+              avatar: avatar,
+              header: header,
+              menu: menu
+            };
+            return array;
+          });
 
-              const opinion = Opinion.docs.map(docs => {
-
-                const { author, clientOpinion, data } = docs.data()
-
-                const sendOpinin = {
-                  author: author,
-                  opinion: clientOpinion,
-                  date: data
-                }
-                return (sendOpinin)
-              })
-
-              const array = {
-                id: id,
-                dishes: dishes,
-                info: info,
-                kitchen: kitchen,
-                opportunity: opportunity,
-                facilities: facilities,
-                avatar: avatar,
-                header: header,
-                menu: menu,
-                clientOpinions: opinion,
-              }
-              res.json({
-                places: array,
-              })
-
-            })
-        });
-      })
+          res.json({
+            places: Places
+          });
+        }
+      });
   });
 });
-
-
 
 module.exports = router;
