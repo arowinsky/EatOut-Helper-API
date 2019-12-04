2f84187673058e95c91f50711e32ad764814d770
Artur Rowinski <arowinski96@gmail.com>
2019-12-04 22:04:15 +0100

    Extension upload eatingPlace images

--- controllers/uploadImagesController.js
+++ controllers/uploadImagesController.js
@@ -1,32 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const uploadImg = require('../upload/upload')
-const redis = require("redis");
-const redisClient = redis.createClient();
+const uploadImg = require("../upload/upload");
+
 router.post("/", async (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-      try{
-          const {idUser, idPlace} = req.body
-          
-          const avatar = req.files.avatar[0];
-          const header = req.files.header[0];
-          const menu = req.files.menu[0];
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-        //console.log(avatar)
+  try {
+    const idUser = req.body.user[0];
+    const idPlace = req.body.user[1];
 
-           await uploadImg(avatar, 'avatar.jpg', idUser, idPlace);
-           await uploadImg(header, 'header.jpg',idUser, idPlace);
-           await uploadImg(menu, 'menu.jpg',idUser, idPlace)
-  
-          res.json({
-              message: "upload udany",
-          })
-      }
-      catch(error){
-        console.log(error)
-        res.send('error')
-      }
-  });
+    const avatar = req.files.photo[0];
+    const header = req.files.photo[1];
+    const menu = req.files.photo[2];
 
+    await uploadImg(avatar, "avatar.jpg", idUser, idPlace);
+    await uploadImg(header, "header.jpg", idUser, idPlace);
+    await uploadImg(menu, "menu.jpg", idUser, idPlace);
+
+    res.json({
+      uploadSuccess: true
+    });
+  } catch (error) {
+    res.send("error");
+  }
+});
 
 module.exports = router;
