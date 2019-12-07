2d459bae76c7fab4a6c11df7796ec5ac0f2aa8f4
Artur Rowinski <arowinski96@gmail.com>
2019-12-07 18:19:03 +0100

    Refactor uploadImagesController

--- controllers/uploadImagesController.js
+++ controllers/uploadImagesController.js
@@ -11,48 +11,47 @@ router.post("/", async (req, res, next) => {
     const avatar = req.files.photo[0];
     const header = req.files.photo[1];
     const menu = req.files.photo[2];
-    
-    if(req.files.photo.length < 3 || req.body.user.length <2){
+
+    if (req.files.photo.length < 3 || req.body.user.length < 2) {
       res.json({
-        noAllImagesSended:true
-      })
-    }
-    else{
-      let statusType = true
+        noAllImagesSended: true
+      });
+    } else {
+      let statusType = true;
 
-      for(let i = 0; i< req.files.photo.length; i++){
-        if(req.files.photo[i].mimetype ===  'image/jpeg' || req.files.photo[i].mimetype ===  'image/png' ||req.files.photo[i].mimetype ===  'image/bmp' ){
-        }
-        else{
-          switch(i){
+      for (let i = 0; i < req.files.photo.length; i++) {
+        if (
+          req.files.photo[i].mimetype === "image/jpeg" ||
+          req.files.photo[i].mimetype === "image/png" ||
+          req.files.photo[i].mimetype === "image/bmp"
+        ) {
+        } else {
+          switch (i) {
             case 0:
-              statusType = false
+              statusType = false;
               res.json({
-              invalidFormatFile: "INVALID_AVATAR",
-              noValidFormatFiles:true,
-              })
-            break;
+                invalidFormatFile: "INVALID_AVATAR"
+              });
+              break;
 
-            case 1 : 
-              statusType = false
+            case 1:
+              statusType = false;
               res.json({
-              invalidFormatFile: "INVALID_HEADER",
-              noValidFormatFiles:true,
-              })
-            break;
+                invalidFormatFile: "INVALID_HEADER"
+              });
+              break;
 
-            case 2 : 
-              statusType = false
+            case 2:
+              statusType = false;
               res.json({
-                invalidFormatFile: "INVALID_MENU",
-                noValidFormatFiles:true,
-              })
-            break;
+                invalidFormatFile: "INVALID_MENU"
+              });
+              break;
           }
-      break;
-    }
-  }
-      if(statusType === true){
+          break;
+        }
+      }
+      if (statusType === true) {
         await uploadImg(avatar, "avatar.jpg", idUser, idPlace);
         await uploadImg(header, "header.jpg", idUser, idPlace);
         await uploadImg(menu, "menu.jpg", idUser, idPlace);
@@ -62,8 +61,8 @@ router.post("/", async (req, res, next) => {
         });
       }
     }
-  }catch (error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
     res.send("error");
   }
 });
