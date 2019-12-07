1c41f9df5e122f4e312229eb70f601d243bd60a9
Artur Rowinski <arowinski96@gmail.com>
2019-12-08 00:27:21 +0100

    Fix bug doesn't send response if user doesn't add all images

--- controllers/uploadImagesController.js
+++ controllers/uploadImagesController.js
@@ -4,52 +4,50 @@ const uploadImg = require("../upload/upload");
 
 router.post("/", async (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
+  let statusType = true;
   try {
-    const idUser = req.body.user[0];
-    const idPlace = req.body.user[1];
-    const avatar = req.files.photo[0];
-    const header = req.files.photo[1];
-    const menu = req.files.photo[2];
-
-    if (req.files.photo.length < 3 || req.body.user.length < 2) {
+    if (req.files.photo === null || req.files.photo === undefined) {
+      statusType = false;
       res.json({
-
-        noAllImagesSended:true
-      })
-    }
-    else{
-      let statusType = true
-     let unavailable = {
+        noAllImagesSended: true
+      });
+    } else {
+      const idUser = req.body.user[0];
+      const idPlace = req.body.user[1];
+      const avatar = req.files.photo[0];
+      const header = req.files.photo[1];
+      const menu = req.files.photo[2];
+      let unavailable = {
         erroravatar: false,
         errorHeader: false,
-        errorMenu: false,
-      }
-      for(let i = 0; i< req.files.photo.length; i++){
-        if(req.files.photo[i].mimetype ===  'image/jpeg' || req.files.photo[i].mimetype ===  'image/png' ||req.files.photo[i].mimetype ===  'image/bmp' ){
-        }
-        else{
-          switch(i){
+        errorMenu: false
+      };
+      for (let i = 0; i < req.files.photo.length; i++) {
+        if (
+          req.files.photo[i].mimetype === "image/jpeg" ||
+          req.files.photo[i].mimetype === "image/png" ||
+          req.files.photo[i].mimetype === "image/bmp"
+        ) {
+        } else {
+          switch (i) {
             case 0:
-              statusType = false
-              unavailable.errorAvatar = true
-            break;
+              statusType = false;
+              unavailable.errorAvatar = true;
+              break;
 
-            case 1 : 
-              statusType = false
-              unavailable.errorHeader = true
-            break;
+            case 1:
+              statusType = false;
+              unavailable.errorHeader = true;
+              break;
 
-            case 2 : 
-              statusType = false
-              unavailable.errorMenu = true
-            break;
+            case 2:
+              statusType = false;
+              unavailable.errorMenu = true;
+              break;
           }
-  
-    }
-  }
-      if(statusType === true){
-
+        }
+      }
+      if (statusType === true) {
         await uploadImg(avatar, "avatar.jpg", idUser, idPlace);
         await uploadImg(header, "header.jpg", idUser, idPlace);
         await uploadImg(menu, "menu.jpg", idUser, idPlace);
@@ -57,16 +55,14 @@ router.post("/", async (req, res, next) => {
         res.json({
           uploadSuccess: true
         });
-      }
-      else{
+      } else {
         res.json({
           invalidFormatFile: unavailable
-        })
+        });
       }
     }
   } catch (error) {
     console.log(error);
-    res.send("error");
   }
 });
 
