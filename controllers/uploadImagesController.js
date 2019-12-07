957596540dc1e50f2ee86f20261a9baf749756b0
Marek <xsior18@gmail.com>
2019-12-07 15:35:12 +0100

    add protection to upload files

--- controllers/uploadImagesController.js
+++ controllers/uploadImagesController.js
@@ -8,19 +8,62 @@ router.post("/", async (req, res, next) => {
   try {
     const idUser = req.body.user[0];
     const idPlace = req.body.user[1];
-
     const avatar = req.files.photo[0];
     const header = req.files.photo[1];
     const menu = req.files.photo[2];
+    
+    if(req.files.photo.length < 3 || req.body.user.length <2){
+      res.json({
+        noAllImagesSended:true
+      })
+    }
+    else{
+      let statusType = true
 
-    await uploadImg(avatar, "avatar.jpg", idUser, idPlace);
-    await uploadImg(header, "header.jpg", idUser, idPlace);
-    await uploadImg(menu, "menu.jpg", idUser, idPlace);
+      for(let i = 0; i< req.files.photo.length; i++){
+        if(req.files.photo[i].mimetype ===  'image/jpeg' || req.files.photo[i].mimetype ===  'image/png' ||req.files.photo[i].mimetype ===  'image/bmp' ){
+        }
+        else{
+          switch(i){
+            case 0:
+              statusType = false
+              res.json({
+              invalidFormatFile: "INVALID_AVATAR",
+              noValidFormatFiles:true,
+              })
+            break;
 
-    res.json({
-      uploadSuccess: true
-    });
-  } catch (error) {
+            case 1 : 
+              statusType = false
+              res.json({
+              invalidFormatFile: "INVALID_HEADER",
+              noValidFormatFiles:true,
+              })
+            break;
+
+            case 2 : 
+              statusType = false
+              res.json({
+                invalidFormatFile: "INVALID_MENU",
+                noValidFormatFiles:true,
+              })
+            break;
+          }
+      break;
+    }
+  }
+      if(statusType === true){
+        await uploadImg(avatar, "avatar.jpg", idUser, idPlace);
+        await uploadImg(header, "header.jpg", idUser, idPlace);
+        await uploadImg(menu, "menu.jpg", idUser, idPlace);
+
+        res.json({
+          uploadSuccess: true
+        });
+      }
+    }
+  }catch (error) {
+    console.log(error)
     res.send("error");
   }
 });
