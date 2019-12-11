99e95b7a5cfcc454e262739b7b495582ef0fce81
Artur Rowinski <arowinski96@gmail.com>
2019-12-11 20:50:21 +0100

    Fix bug with return wrong time zone in ownerPost & clientOpinion

--- controllers/addPostOwnerController.js
+++ controllers/addPostOwnerController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { db, admin} = require("../config/firebaseConfig");
+const { db, admin } = require("../config/firebaseConfig");
 
 router.post("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,50 +13,47 @@ router.post("/", (req, res) => {
     .collection("postsOwner")
     .doc()
     .set({ post: post, author: nameLocal, date: new Date() })
-    .then((doc) => {
+    .then(doc => {
       db.collection("eatingPlaces")
-      .doc(local)
-      .collection('postsOwner')
-      .orderBy('date', "desc")
-      .get()
-      .then(posts => {
-       const postsOwner = posts.docs.map(doc => {
-        const {
-            author,
-            post,
-            date,
-          } = doc.data();
+        .doc(local)
+        .collection("postsOwner")
+        .orderBy("date", "desc")
+        .get()
+        .then(posts => {
+          const postsOwner = posts.docs.map(doc => {
+            const { author, post, date } = doc.data();
 
+            const newDate = date.toDate();
+            const DateString = newDate.toISOString();
+            const year = DateString.substr(0, 4);
+            const month = DateString.substr(5, 2);
+            const day = DateString.substr(8, 2);
+            let hour = DateString.substr(11, 2);
+            hour = parseInt(hour, 10);
+            hour++;
+            hour.toString();
+            const minute = DateString.substr(14, 2);
+            const sendDate =
+              day + "." + month + "." + year + " " + hour + ":" + minute;
 
-              const newDate = date.toDate();
-              const DateString = newDate.toISOString()
-              const year = DateString.substr(0,4);
-              const month = DateString.substr(5,2)
-              const day = DateString.substr(8,2)
-              const hour = DateString.substr(11,2)
-              const minute = DateString.substr(14,2)
-              const sendDate= day+'.'+ month+'.'+year +' '+ hour+':'+minute;
+            const array = {
+              author: author,
+              post: post,
+              date: sendDate
+            };
+            return array;
+          });
 
-              const array = {
-                author: author,
-                post: post,
-                date: sendDate
-              }
-              return(array)
-
-        })
-
-        res.json({
-          addedPost: true,
+          res.json({
+            addedPost: true,
             postsForCurrentProfile: postsOwner
-        })
-      })
-
+          });
+        });
     })
-    .catch(error=>{
+    .catch(error => {
       res.json({
-        error:error
-      })
+        error: error
+      });
     });
 });
 module.exports = router;
