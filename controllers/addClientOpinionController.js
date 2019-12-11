3c9cf42d1a2940a96c020a12f3dd3b72107a62ae
Marek <xsior18@gmail.com>
2019-12-11 16:26:23 +0100

    remove 2 controllers and rebuild 2 controllers

--- controllers/addClientOpinionController.js
+++ controllers/addClientOpinionController.js
@@ -26,9 +26,47 @@ router.post("/", (req, res) => {
           date: new Date()
         })
         .then(() => {
-          res.json({
-            isAdded: true
-          });
+
+          db.collection("eatingPlaces")
+          .doc(eatingPlaceId)
+          .collection('clientOpinions')
+          .orderBy('date', "desc")
+          .get()
+          .then(opinion => {
+              if(opinion.empty){
+                  console.log('empty')
+              }
+           const clientOpinion = opinion.docs.map(doc => {
+                const {
+                    author,
+                    clientOpinion,
+                    date,
+                  } = doc.data();
+    
+    
+                  const newDate = date.toDate();
+                  const DateString = newDate.toISOString()
+                  const year = DateString.substr(0,4);
+                  const month = DateString.substr(5,2)
+                  const day = DateString.substr(8,2)
+                  const hour = DateString.substr(11,2)
+                  const minute = DateString.substr(14,2)
+                  const sendDate= day+'.'+ month+'.'+year +' '+ hour+':'+minute;
+    
+                  const array = {
+                    author: author,
+                    clientOpinion: clientOpinion,
+                    date: sendDate
+                  }
+                  return(array)
+    
+            })
+    
+            res.json({
+              isAdded: true,
+                opinionsForCurrentProfile: clientOpinion
+            })
+          })
         });
     } catch (error) {
       res.json({
--- controllers/addPostOwnerController.js
+++ controllers/addPostOwnerController.js
@@ -13,10 +13,45 @@ router.post("/", (req, res) => {
     .collection("postsOwner")
     .doc()
     .set({ post: post, author: nameLocal, date: new Date() })
-    .then(() => {
-      res.json({
-        addedPost: true
-      });
+    .then((doc) => {
+      db.collection("eatingPlaces")
+      .doc(local)
+      .collection('postsOwner')
+      .orderBy('date', "desc")
+      .get()
+      .then(posts => {
+       const postsOwner = posts.docs.map(doc => {
+        const {
+            author,
+            post,
+            date,
+          } = doc.data();
+
+
+              const newDate = date.toDate();
+              const DateString = newDate.toISOString()
+              const year = DateString.substr(0,4);
+              const month = DateString.substr(5,2)
+              const day = DateString.substr(8,2)
+              const hour = DateString.substr(11,2)
+              const minute = DateString.substr(14,2)
+              const sendDate= day+'.'+ month+'.'+year +' '+ hour+':'+minute;
+
+              const array = {
+                author: author,
+                post: post,
+                date: sendDate
+              }
+              return(array)
+
+        })
+
+        res.json({
+          addedPost: true,
+            postsForCurrentProfile: postsOwner
+        })
+      })
+
     })
     .catch(error=>{
       res.json({
