b213503cf0e3e1091094555515959a0c08939631
Artur Rowinski <arowinski96@gmail.com>
2019-12-15 23:20:48 +0100

    Refactor structure controllers

--- controllers/getDataPlace/getDataPlaceController.js
+++ controllers/getDataPlace/getDataPlaceController.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const redis = require("redis");
 const redisClient = redis.createClient();
-const { db, admin, auth } = require("../config/firebaseConfig");
-const getPlace = require("../getAll/getPlace");
-const getOpinion = require("../getAll/getClientOpinon");
-const getPostsOwner = require("../getAll/getPostsOwner");
+const { db, admin, auth } = require("../../config/firebaseConfig");
+const getPlace = require("../../getAll/getPlace");
+const getOpinion = require("../../getAll/getClientOpinon");
+const getPostsOwner = require("../../getAll/getPostsOwner");
 router.post("/", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
