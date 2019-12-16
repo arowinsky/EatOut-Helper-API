1c3e9e1b530e5f132c80190070e164ee25a4b28f
Artur Rowinski <arowinski96@gmail.com>
2019-12-16 11:21:02 +0100

    Changes: * Install & configure nodemon in app * Refactor view autoLoginController

--- controllers/autoLogin/autoLoginController.js
+++ controllers/autoLogin/autoLoginController.js
@@ -11,49 +11,40 @@ router.post("/", (req, res) => {
   if (id != "null") {
     if (id != "undefined" && reSend) {
       const key = "sess:" + id;
-
       redisClient.get(key, (err, date) => {
-        try{
-        const dates = JSON.parse(date);
-        const userData = dates.userData;
-        if (err === null) {
-          console.log("Resend" + userData);
+        try {
+          const dates = JSON.parse(date);
+          const userData = dates.userData;
+          if (err === null) {
+            console.log("Resend" + userData);
+            res.json({
+              userInfo: userData,
+              userId: dates.localId
+            });
+          }
+          if (err != null) {
+            console.log("error");
+            res.json({
+              session: "TimeOut"
+            });
+          }
+        } catch (e) {
+          console.log("bład");
           res.json({
-            userInfo: userData,
-            userId: dates.localId
+            session: "wrong_Z"
           });
         }
-        if (err != null) {
-          console.log("error");
-          res.json({
-            session: "TimeOut"
-          });
-        }
-      }
-      catch(e){
-        console.log('bład')
-      res.json({
-        session:"wrong_Z"
-      })
-      }
-
       });
-
-
-    }
-     else {
+    } else {
       res.json({
         session: "TimeOut"
       });
     }
-  }
-
-   else {
+  } else {
     console.log("lack of sid");
     res.json({
       session: "TimeOut"
     });
   }
-
 });
 module.exports = router;
