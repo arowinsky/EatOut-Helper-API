9b26c1fbbe0c169c776379fcf8abc8f5c8eec457
MarekLandzwojczak <mareklandzwojczak1995@gmail.com>
2020-01-09 13:54:24 +0100

    add uid in object

--- controllers/followPlaces/addToFollowController.js
+++ controllers/followPlaces/addToFollowController.js
@@ -5,39 +5,40 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 
 router.post("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-const key = 'sess:' + req.body.z;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const key = 'sess:' + req.body.z;
 
 
-redisClient.get(key, (err, data) => {
+    redisClient.get(key, (err, data) => {
 
-    const user = JSON.parse(data);
-    const PlaceId = req.body.placeId;
-const PlaceName = req.body.placeName
+        const user = JSON.parse(data);
+        const PlaceId = req.body.placeId;
+        const PlaceName = req.body.placeName
 
-let followPlace = {
-    placeId: PlaceId,
-    placeName:PlaceName, 
-}
-console.log('2')
-    db.collection('users')
-    .doc(user.localId)
-    .collection('follow')
-    .doc()
-    .set(followPlace)
-    .then(()=>{
-        res.json({
-            addFollow: true
-        })
+        let followPlace = {
+            placeId: PlaceId,
+            placeName: PlaceName,
+            uid: user.localId,
+        }
+
+        db.collection('users')
+            .doc(user.localId)
+            .collection('follow')
+            .doc()
+            .set(followPlace)
+            .then(() => {
+                res.json({
+                    addFollow: true
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                res.json({
+                    addFollow: false
+                })
+            })
+
     })
-    .catch(error=>{
-        console.log(error);
-        res.json({
-            addFollow: false
-        })
-    })
-
-})
 
 });
 module.exports = router; 
--- controllers/followPlaces/removeFromFollowController.js
+++ controllers/followPlaces/removeFromFollowController.js
@@ -5,54 +5,52 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 
 router.post("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-const key = 'sess:' + req.body.z;
-const placeId = req.body.placeId;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const key = 'sess:' + req.body.z;
+    const placeId = req.body.placeId;
 
-redisClient.get(key, (err, data) => {
+    redisClient.get(key, (err, data) => {
 
-    const user = JSON.parse(data);
-    console.log(placeId)
+        const user = JSON.parse(data);
 
-    db.collection('users')
-    .doc(user.localId)
-    .collection('follow')
-    .where('placeId', '==', placeId)
-    .get()
-    .then((docs)=>{
-        
-        if(docs.empty)
-        {
-            res.json({
-                deleteFollow: 'error'
-            })
-        }
-        else{
-            docs.forEach(doc =>{
-            db.collection('users')
+        db.collection('users')
             .doc(user.localId)
             .collection('follow')
-            .doc(doc.id)
-            .delete()
-            .then(()=>{
-                res.json({deleteFollow:true})
+            .where('placeId', '==', placeId)
+            .get()
+            .then((docs) => {
+
+                if (docs.empty) {
+                    res.json({
+                        deleteFollow: 'error'
+                    })
+                }
+                else {
+                    docs.forEach(doc => {
+                        db.collection('users')
+                            .doc(user.localId)
+                            .collection('follow')
+                            .doc(doc.id)
+                            .delete()
+                            .then(() => {
+                                res.json({ deleteFollow: true })
+                            })
+                            .catch(error => {
+                                console.log(error)
+                                res.json({ deleteFollow: false })
+                            })
+
+                    })
+                }
             })
-            .catch(error=>{
-                console.log(error)
-                res.json({deleteFollow:false})
+            .catch(error => {
+                console.log(error);
+                res.json({
+                    deleteFollow: false
+                })
             })
 
-            })
-        }
     })
-    .catch(error=>{
-        console.log(error);
-        res.json({
-            deleteFollow: false
-        })
-    })
-
-})
 
 });
 module.exports = router; 
