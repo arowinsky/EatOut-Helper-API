3cfab523b2d8a3873b9d712f797ad44c3e3dd0d1
Marek <xsior18@gmail.com>
2020-01-21 17:59:30 +0100

    resolve problems

---
+++ getAll/getClientOpinon.js
@@ -0,0 +1,47 @@
+const { db, admin } = require("../config/firebaseConfig");
+
+const opinion = places =>
+  new Promise((resolve, reject) => {
+    for (let i = 0; i < places.length; i++) {
+      console.log("wchodze");
+      db.collection("eatingPlaces")
+        .doc(places[i].id)
+        .collection("clientOpinions")
+        .orderBy("date", "desc")
+        .get()
+        .then(post => {
+          if (post.empty) {
+            console.log("empty opinion");
+            places[i].clientsOpinions = false;
+            const newPlace = places;
+            resolve(newPlace);
+          } else {
+            const clientOpinion = post.docs.map(doc => {
+              const { author, clientOpinion, date } = doc.data();
+
+              const newDate = date.toDate();
+              const DateString = newDate.toISOString();
+              const year = DateString.substr(0, 4);
+              const month = DateString.substr(5, 2);
+              const day = DateString.substr(8, 2);
+              const hour = DateString.substr(11, 2);
+              const minute = DateString.substr(14, 2);
+              const sendDate =
+                day + "." + month + "." + year + " " + hour + ":" + minute;
+
+              const array = {
+                author: author,
+                clientOpinion: clientOpinion,
+                date: sendDate
+              };
+              return array;
+            });
+
+            places[i].clientsOpinions = clientOpinion;
+            const NewPlace = places;
+            resolve(NewPlace);
+          }
+        });
+    }
+  });
+module.exports = opinion;
