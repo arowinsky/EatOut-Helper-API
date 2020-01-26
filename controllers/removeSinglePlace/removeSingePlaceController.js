d56c6afeb1c856eaf6c3884e6656a9c1d5b1bb48
Artur Rowinski <arowinski96@gmail.com>
2020-01-26 20:54:53 +0100

    Change method DELETE to POST

--- controllers/removeSinglePlace/removeSingePlaceController.js
+++ controllers/removeSinglePlace/removeSingePlaceController.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const redis = require("redis");
 const redisClient = redis.createClient();
 const { db, admin, auth } = require("../../config/firebaseConfig");
-const deleteImg = require('./deleteImg');
-router.delete("/", (req, res) => {
+const deleteImg = require("./deleteImg");
+router.post("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  console.log(req.body);
+  console.log("Remove place", req.body);
   const z = req.body.z;
   const id = req.body.id;
 
@@ -15,39 +15,32 @@ router.delete("/", (req, res) => {
 
   redisClient.get(key, (err, red) => {
     red = JSON.parse(red);
+    console.log("LokalId w redisie", red.localId);
+    db.collection("eatingPlaces")
+      .doc(id)
+      .get()
+      .then(doc => {
+        if (red.localId === doc.data().info.owner) {
+          db.collection("eatingPlaces")
+            .doc(id)
+            .delete()
+            .then(async () => {
+              await deleteImg("avatar.jpg", red.localId, id);
+              await deleteImg("header.jpg", red.localId, id);
+              await deleteImg("menu.jpg", red.localId, id);
 
-    db.collection('eatingPlaces').doc(id).get()
-    .then(doc=>{
-   
-
-        if(red.localId === doc.data().info.owner ){
-
-            db.collection('eatingPlaces').doc(id).delete().then(async()=>{
-
-                await deleteImg('avatar.jpg', red.localId, id)
-                await deleteImg('header.jpg', red.localId, id)
-                await deleteImg('menu.jpg', red.localId, id)
-
-                res.json({
-                    removePlace: true,
-                })
-            })
-
+              res.json({
+                removePlace: true
+              });
+            });
+        } else {
+          res.json({
+            removePlace: false,
+            message: "You are not the onwer of this place"
+          });
         }
-        else{
-            res.json({
-                removePlace: false,
-                message: 'You are not the onwer of this place'
-            })
-        }
-
-    })
-
-
-
-
+      });
   });
 });
 
 module.exports = router;
-     
