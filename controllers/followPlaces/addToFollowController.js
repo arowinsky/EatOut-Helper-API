af802d1dcbde4d2de88d7d00c0ca71fd5b5380c8
Artur Rowinski <arowinski96@gmail.com>
2020-02-09 12:24:18 +0100

    Reafctor addToFollowController

--- controllers/followPlaces/addToFollowController.js
+++ controllers/followPlaces/addToFollowController.js
@@ -5,47 +5,46 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 
 router.post("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const { z } = req.body;
+  const key = "sess:" + z;
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    const key = 'sess:' + req.body.z;
+  redisClient.get(key, (err, data) => {
+    try {
+      const user = JSON.parse(data);
+      const { placeId, placeName } = req.body;
 
-
-    redisClient.get(key, (err, data) => {
-try{
-        const user = JSON.parse(data);
-        const PlaceId = req.body.placeId;
-        const PlaceName = req.body.placeName
-
-        db.collection('eatingPlaces').doc(PlaceId).get()
-        .then(place=>{
-            const owner = place.data().info.owner
-            let followPlace = {
-                placeId: PlaceId,
-                placeName: PlaceName,
-                uid: user.localId,
-                avatar: `https://storage.cloud.google.com/eatout/${owner}/${PlaceId}/avatar.jpg`
-            }
-        db.collection('users')
+      db.collection("eatingPlaces")
+        .doc(placeId)
+        .get()
+        .then(place => {
+          const owner = place.data().info.owner;
+          let followPlace = {
+            placeId: placeId,
+            placeName: placeName,
+            uid: user.localId,
+            avatar: `https://storage.cloud.google.com/eatout/${owner}/${placeId}/avatar.jpg`
+          };
+          db.collection("users")
             .doc(user.localId)
-            .collection('follow')
+            .collection("follow")
             .doc()
             .set(followPlace)
             .then(() => {
-                res.json({
-                    userFollowing:true
-                })
+              res.json({
+                userFollowing: true
+              });
             })
             .catch(error => {
-                console.log(error);
-                res.json({
-                    userFollowing:false
-                })
-            })
-        })
+              console.log(error);
+              res.json({
+                userFollowing: false
+              });
+            });
+        });
+    } catch (error) {
+      console.log("wrong z");
     }
-    catch(error){
-        console.log('wrong z')
-    }
-    })
+  });
 });
 module.exports = router;
