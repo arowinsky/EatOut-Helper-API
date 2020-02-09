96e6d94486052b330f4bcf7ba49e57716136a5c8
Artur Rowinski <arowinski96@gmail.com>
2020-02-10 00:30:38 +0100

    Refactor getDataPlaceController

--- controllers/getDataPlace/getDataPlaceController.js
+++ controllers/getDataPlace/getDataPlaceController.js
@@ -6,26 +6,24 @@ const { db, admin, auth } = require("../../config/firebaseConfig");
 const getPlace = require("../../getAll/getPlace");
 const getOpinion = require("../../getAll/getClientOpinon");
 const getPostsOwner = require("../../getAll/getPostsOwner");
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  const z = req.body.z;
+  const z = req.query.z;
   const key = "sess:" + z;
   redisClient.get(key, async (err, red) => {
     red = JSON.parse(red);
     const Place = await getPlace(red.localId);
-    if(Place != null){
+    if (Place != null) {
       const Opinion = await getOpinion(Place);
       const Posts = await getPostsOwner(Opinion);
       res.json({
         places: Posts
       });
+    } else {
+      res.json({
+        places: null
+      });
     }
-    else{
-    res.json({
-      places: null
-    });
-
-  }
   });
 });
 
