01ade171a1743ae40d5f7aa718380d1480bdf075
Artur Rowinski <arowinski96@gmail.com>
2020-02-10 00:39:49 +0100

    Improve code readability

--- controllers/getDataPlaceSingle/getDataPlaceSingleController.js
+++ controllers/getDataPlaceSingle/getDataPlaceSingleController.js
@@ -9,45 +9,43 @@ const getPostsOwner = require("../../getSingle/getPostOwnerSingle");
 router.get("/", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const {placeId,z} = req.query;
+  const { placeId, z } = req.query;
   const Place = await getPlace(placeId);
   const Opinion = await getOpinion(Place);
   const Posts = await getPostsOwner(Opinion);
-  if (z === 'null' || z === 'undefined'){
-
+  if (z === "null" || z === "undefined") {
     res.json({
       place: Posts
     });
-  }
-  else{
-const key = "sess:"+z
-  redisClient.get(key, (error, data)=>{
-    try{
-    data = JSON.parse(data);
+  } else {
+    const key = "sess:" + z;
+    redisClient.get(key, (error, data) => {
+      try {
+        data = JSON.parse(data);
 
-    db.collection('users').doc(data.localId).collection('follow').where("placeId", '==', placeId).get()
-    .then(follow =>{
-      if(follow.empty){
-        Posts.following = false
-        res.json({
-          place: Posts
-        });
+        db.collection("users")
+          .doc(data.localId)
+          .collection("follow")
+          .where("placeId", "==", placeId)
+          .get()
+          .then(follow => {
+            if (follow.empty) {
+              Posts.following = false;
+              res.json({
+                place: Posts
+              });
+            } else {
+              Posts.following = true;
+              res.json({
+                place: Posts
+              });
+            }
+          });
+      } catch (error) {
+        console.log("error redis");
       }
-      else {
-        Posts.following = true
-        res.json({
-          place: Posts
-        });
-      }
-    })
+    });
   }
-  catch(error){
-    console.log('error redis')
-  }
-  })
-}
-
-
 });
 
 module.exports = router;
