13cf53fbb81839bcc81d1241bfc2946991e191dc
Artur Rowinski <arowinski96@gmail.com>
2020-02-11 00:21:43 +0100

    Refactor verificationEmailController

--- controllers/userAction/verificationEmailController.js
+++ controllers/userAction/verificationEmailController.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const { db, admin, auth } = require("../../config/firebaseConfig");
 router.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
   const { mode, oobCode } = req.query;
-
   auth
     .applyActionCode(oobCode)
     .then(resp => {
-
       res.json({
         status: true
       });
