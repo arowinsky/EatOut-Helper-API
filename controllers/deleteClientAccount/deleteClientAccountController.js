572dd0e291981af89280555e48a65f7584cb41fc
Artur Rowinski <arowinski96@gmail.com>
2020-02-10 12:11:36 +0100

    Refactor deleteClientAccountController

--- controllers/deleteClientAccount/deleteClientAccountController.js
+++ controllers/deleteClientAccount/deleteClientAccountController.js
@@ -4,43 +4,36 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 const { db, admin, auth } = require("../../config/firebaseConfig");
 router.delete("/:z", (req, res) => {
-
-console.log('udało się')
-  console.log("z teraz:", req.params.z);
   const { z } = req.params;
-
   const key = "sess:" + z;
-  console.log(key);
   redisClient.get(key, (err, data) => {
     try {
-      console.log("przeszło z ", z)
-      // data = JSON.parse(data);
-
-      // db.collection("users")
-      //   .doc(data.localId)
-      //   .collection("follow")
-      //   .get()
-      //   .then(follow => {
-      //     follow.forEach(follows => {
-      //       db.collection("users")
-      //         .doc(data.localId)
-      //         .collection("follow")
-      //         .doc(follows.id)
-      //         .delete();
-      //     });
-      //   });
-      // db.collection("users")
-      //   .doc(data.localId)
-      //   .delete();
-      // admin
-      //   .auth()
-      //   .deleteUser(data.localId)
-      //   .then(() => {
-      //     redisClient.del(key);
-      //     res.json({
-      //       clientDeleted: true
-      //     });
-      //   });
+      data = JSON.parse(data);
+      db.collection("users")
+        .doc(data.localId)
+        .collection("follow")
+        .get()
+        .then(follow => {
+          follow.forEach(follows => {
+            db.collection("users")
+              .doc(data.localId)
+              .collection("follow")
+              .doc(follows.id)
+              .delete();
+          });
+        });
+      db.collection("users")
+        .doc(data.localId)
+        .delete();
+      admin
+        .auth()
+        .deleteUser(data.localId)
+        .then(() => {
+          redisClient.del(key);
+          res.json({
+            clientDeleted: true
+          });
+        });
     } catch (error) {
       console.log(error);
     }
